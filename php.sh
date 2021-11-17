7593f821ea17a6b539302aa55cb4f933abee8f75
Marcin <marcin@breakermind.com>
2021-11-17 16:13:41 +0100

    Lemp, mysql, smtp, php8.0
    Linux vps server with php8.0, mysql, nginx, postfix, firewall ...

--- php.sh
+++ php.sh
@@ -4,7 +4,6 @@ sudo apt install -y apt-transport-https
 sed -i 's/http\:/https\:/g' /etc/apt/sources.list
 
 sudo apt update -y
-
 sudo apt install -y curl wget gnupg2 ca-certificates lsb-release software-properties-common
 
 sudo curl https://packages.sury.org/php/apt.gpg | gpg --dearmor > /usr/share/keyrings/sury-php.gpg
@@ -13,12 +12,32 @@ sudo echo "deb [signed-by=/usr/share/keyrings/sury-php.gpg] https://packages.sur
 sudo apt update -y
 sudo apt upgrade -y
 
+# vps
+sudo apt install -y net-tools dnsutils mailutils git composer ufw redis memcached nginx mariadb-server postfix
+
+# php
 sudo apt install -y php8.0-fpm
 sudo apt install -y php8.0-{mysql,xml,curl,mbstring,opcache,gd,imagick,imap,bcmath,bz2,zip,intl,redis,memcache,memcached}
+sudo service php8.0-fpm restart
 
 sudo update-alternatives --set php /usr/bin/php8.0
 
-exit 1;
+# Php
+# sudo php -v
+# sudo php -m
+
+# Firewall
+# sudo ufw allow from 5.6.0.0/16 to any port 22 proto tcp
+# sudo ufw allow 22/tcp
+# sudo ufw allow 80/tcp
+# sudo ufw allow 443/tcp
+# sudo ufw logging on
+# sudo ufw default allow outgoing
+# sudo ufw default deny incoming
+# sudo ufw enable
+
+exit 1
+
 # sudo apt install -y php7.4-fpm
 # sudo apt install -y php7.4-{mysql,json,xml,curl,mbstring,opcache,gd,imagick,imap,bcmath,bz2,zip,intl,redis,memcache,memcached}
 # sudo update-alternatives --list php
